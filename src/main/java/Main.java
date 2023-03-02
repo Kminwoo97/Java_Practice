a27c9019a302226dcca2513a8b14b34f5d4c86e5
Kminwoo97 <kmo971130@gmail.com>
2023-03-02 09:28:37 +0900

    2단계 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         System.out.println("== 명언앱 -- ");
+
+
         String op;
+        String title;
+        String author;
         while(true){
-            System.out.print("명령) ");
+            System.out.print("명령 ) ");
             st = new StringTokenizer(br.readLine());
+
             op = st.nextToken();
             if (op.equals("종료")) {
                 break;
+            } else if (op.equals("등록")) {
+                System.out.print("명언 : ");
+                st = new StringTokenizer(br.readLine());
+                title = st.nextToken();
+                System.out.print("작가 : ");
+                st = new StringTokenizer(br.readLine());
+                author = st.nextToken();
             }
-
         }
     }
 }
